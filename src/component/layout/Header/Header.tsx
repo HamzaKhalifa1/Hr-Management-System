da1969db32aeab972500c037fd29a612f521a334
HamzaKhalifa1 <enghamzakha@gmail.com>
2025-02-01 23:33:23 +0200

    Refactor Header component: add styles, search functionality, and improved layout

--- src/component/layout/Header/Header.tsx
+++ src/component/layout/Header/Header.tsx
@@ -1,6 +1,39 @@
+import styles from "./Header.module.css"
+import {useState} from "react";
+import search from "../../../assets/icons/search02.svg";
 
 export default function Header() {
-  return (
-    <div>Header</div>
+    const [value, setValue] = useState('');
+    const handleChange = (e:any) => {
+        setValue(e.target.value);
+    };
+    return (
+    <header >
+      <section className={styles.leftHeader}>
+        <h6>Header</h6>
+        </section>
+        <section className={styles.rightHeader}>
+            <div className={styles.searchWrapper}>
+                <div className={styles.searchIcon}>
+                    <div >
+                        <img src={search} alt="search" />
+                    </div>
+                </div>
+                <input
+                    type="text"
+                    value={value}
+                    onChange={handleChange}
+                    placeholder="Search"
+                    className={styles.searchInput}
+                />
+            </div>
+              <div >
+                  <h6>Header</h6>
+              </div>
+              <div >
+                  <h6>Header</h6>
+              </div>
+        </section>
+    </header>
   )
 }
